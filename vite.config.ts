be7502b8bc8b0ed357915b94ba6310f01d762746
jaeh <jascha@jaeh.at>
2023-09-18 11:52:08 +0200

    vite.config.js: add manualChunks, change port to 8000

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,32 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vitest/config'
 
+const manualChunks = (id : string) => {
+  if (id.includes('node_modules') || id.includes('/lib/') || id.includes('/routes/')) {
+    return 'vendor'
+  }
+
+  console.log(id)
+  return 'index'
+}
+
 export default defineConfig({
   plugins: [sveltekit()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks,
+      },
+    },
+  },
+  server: {
+    port: 8000,
+    strictPort: false,
+  },
+  preview: {
+    port: 8000,
+    strictPort: false,
+  },
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
   },
