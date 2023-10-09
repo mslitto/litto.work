0d979bf658fbfa60278c1f25bda43db611c9f48c
jaeh <jascha@jaeh.at>
2023-10-09 22:09:49 +0200

    format

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
-export let href : string
-export let nofollow : boolean = false
-export let noreferrer : boolean = false
+  export let href: string
+  export let nofollow: boolean = false
+  export let noreferrer: boolean = false
 
-const isLocal : boolean = href[0] === '/' || href[0] === '#'
+  const isLocal: boolean = href[0] === '/' || href[0] === '#'
 
-const target : string | null = isLocal ? null : '_blank'
-let rel: string | undefined = undefined
+  const target: string | null = isLocal ? null : '_blank'
+  let rel: string | undefined = undefined
 
-if (!isLocal) {
-  rel = 'noopener'
+  if (!isLocal) {
+    rel = 'noopener'
 
-  if (nofollow) {
-    rel += ' nofollow'
+    if (nofollow) {
+      rel += ' nofollow'
+    }
+    if (noreferrer) {
+      rel += ' noreferrer'
+    }
   }
-  if (noreferrer) {
-    rel += ' noreferrer'
-  }
-}
 </script>
 
 <a {href} {rel} {target}><slot /></a>
--- src/components/Picture.svelte
+++ src/components/Picture.svelte
@@ -1,43 +1,42 @@
 <script lang="ts">
-import { is } from '@lib'
+  import { is } from '@lib'
 
-export let height : string | number
-export let width: string | number
-export let role : string | null = null
-export let alt : string | null = null
-export let avif : boolean = false
-export let webp : boolean = true
-export let src : string
-export let imgClass : string | null = null
-export let pictureClass : string | null = 'Picture'
+  export let height: string | number
+  export let width: string | number
+  export let role: string | null = null
+  export let alt: string | null = null
+  export let avif: boolean = false
+  export let webp: boolean = true
+  export let src: string
+  export let imgClass: string | null = null
+  export let pictureClass: string | null = 'Picture'
 
-if ((alt === '' && !role) || !is.str(role)) {
-  role = 'presentation'
-}
+  if ((alt === '' && !role) || !is.str(role)) {
+    role = 'presentation'
+  }
 
-const srcsets: string[] = []
+  const srcsets: string[] = []
 
-if (webp !== false) {
-  srcsets.push(src.replace(/\.(jpg|png|gif)$/, '.webp'))
-}
+  if (webp !== false) {
+    srcsets.push(src.replace(/\.(jpg|png|gif)$/, '.webp'))
+  }
 
-// avif is disabled by default
-if (avif !== false) {
-  srcsets.push(src.replace(/\.(jpg|png|gif)$/, '.webp'))
-}
+  // avif is disabled by default
+  if (avif !== false) {
+    srcsets.push(src.replace(/\.(jpg|png|gif)$/, '.webp'))
+  }
 
-if (!pictureClass.includes('Picture')) {
-  pictureClass = `Picture ${pictureClass ? pictureClass : undefined}`
-}
-
+  if (!pictureClass.includes('Picture')) {
+    pictureClass = `Picture ${pictureClass ? pictureClass : undefined}`
+  }
 </script>
 
-<picture class="{pictureClass}">
+<picture class={pictureClass}>
   {#each srcsets as srcset}
-    <source srcset={srcset} />
+    <source {srcset} />
   {/each}
 
-  <img class="{imgClass}" {src} {alt} {role} {height} {width} />
+  <img class={imgClass} {src} {alt} {role} {height} {width} />
 </picture>
 
 <style>
