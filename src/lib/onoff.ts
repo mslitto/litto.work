1b5adedcbfe70c8df67aec37fbb45cb5a1b978a4
jaeh <jascha@jaeh.at>
2023-10-22 16:27:16 +0200

    lib/onoff: fix types

--- src/lib/onoff.ts
+++ src/lib/onoff.ts
@@ -1,8 +1,21 @@
 export type Ele = HTMLElement | Document | Window
-export type Cb = (evt: Event | MouseEvent) => boolean | void
+export type Cb = (evt: Event) => boolean | void
+export type EventOptions = {
+  passive?: boolean;
+  once?: boolean;
+  capture?: boolean;
+}
 
-export const on = (ele: Ele, listener: string, cb: Cb): void =>
-  ele && ele.addEventListener(listener, cb)
+export const on = (
+  ele: EventTarget,
+  listener: string,
+  cb: Cb,
+  options: EventOptions | undefined = undefined,
+): void => ele && ele.addEventListener(listener, cb, options)
 
-export const off = (ele: Ele, listener: string, cb: Cb): void =>
-  ele && ele.removeEventListener(listener, cb)
+export const off = (
+  ele: EventTarget,
+  listener: string,
+  cb: Cb,
+  options: EventOptions | undefined = undefined,
+): void => ele && ele.removeEventListener(listener, cb, options)
